13433e4f3f59020f9911a04b94046881cfade31c
andhycoder <amarzoni050@gmail.com>
2026-08-04 09:13:53 +0800

    apps-1: menambahkan struct ke main.rs
    fitur yang akan datang:
    1. membuat menu untuk menambahkan data siswa
    2. membuat fungdi untuk menyimpan data siswa

--- apps-1/src/main.rs
+++ apps-1/src/main.rs
@@ -1,3 +1,52 @@
+// TODO:
+// fitur yang ditambahkan:
+// 1. membuat struct `KartuSiswa`
+// 2. membuat getter & setter pads `KartuSiswa`
+
+#[derive(Debug)]
+struct KartuSiswa {
+    name: String,
+    nisn: i32,
+}
+
+impl KartuSiswa {
+    const fn new(name: String, nisn: i32) -> Self {
+        Self { name, nisn }
+    }
+
+    /// getter method for name
+    const fn name(&self) -> &String {
+        &self.name
+    }
+
+    /// setter method for name
+    fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+
+    /// getter method for nisn
+    const fn nisn(&self) -> i32 {
+        self.nisn
+    }
+
+    /// setter method for nisn
+    const fn set_nisn(&mut self, nisn: i32) {
+        self.nisn = nisn;
+    }
+
+    fn info(&self) {
+        println!("nama : {}", self.name());
+        println!("nisn : {}", self.nisn());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    #[allow(clippy::unreadable_literal)]
+    let mut kartu = KartuSiswa::new(String::new(), 0);
+
+    kartu.set_name("bambang".to_string());
+    #[allow(clippy::unreadable_literal)]
+    kartu.set_nisn(308442715);
+
+    kartu.info();
 }
